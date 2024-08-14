43f5b70cc81d6f80b8d11e59c860b0911fc9217b
obiorahm <mmachiobiorah2015@u.northwestern.edu>
2024-08-14 18:41:58 -0400

    set background-size: vw vh

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const MIN_LIFE_SPAN = 10;
 const MAX_LIFE_SPAN = 50; // actually 100 + 500
-const MAX_NUMBER_OF_BIRDS = 20;
+const MAX_NUMBER_OF_BIRDS = 8;
 // const WINDOW_WIDTH = window.innerWidth;
 // const WINDOW_HEIGHT = window.innerHeight;
 const WINDOW_WIDTH = window.innerWidth / 2;
