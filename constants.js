c02ceb1843464a3acba5499b12dac16c155bd1f8
obiorahm <mmachiobiorah2015@u.northwestern.edu>
2024-08-21 17:32:11 -0400

    use min of screen height and width

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ const MAX_LIFE_SPAN = 50; // actually 100 + 500
 const MAX_NUMBER_OF_BIRDS = 8;
 // const WINDOW_WIDTH = window.innerWidth;
 // const WINDOW_HEIGHT = window.innerHeight;
-const WINDOW_WIDTH = window.innerHeight / 2;
+const WINDOW_WIDTH = Math.min(window.innerWidth, window.innerHeight) / 2;
 const WINDOW_HEIGHT = window.innerHeight / 2;
 console.log("window width: " + WINDOW_WIDTH);
 console.log("window height: " + WINDOW_HEIGHT);
@@ -12,7 +12,7 @@ const MIN_HEIGHT = WINDOW_HEIGHT;
 const MIN_WIDTH = WINDOW_WIDTH;
 
 const HEIGHT_OFFSET = window.innerHeight / 4;
-const WIDTH_OFFSET = window.innerWidth / 4;
+const WIDTH_OFFSET = Math.min(window.innerWidth, window.innerHeight) / 4;
 
 const MAX_MOVEMENT = 5;
 const MIN_MOVEMENT = -5;
