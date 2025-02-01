27450a74c5afd2d9389c52590b5e04df787c4af9
Mmachi Obiorah <mobiorah@m1-mcs-mobiorah.richmond.edu>
2025-01-31 19:12:18 -0500

    maze added

---
+++ maze/script.js
@@ -0,0 +1,8 @@
+(function(){
+    // console.log(state[0]);
+    // let testElement = document.createElement("div");
+    // testElement.style.backgroundColor = "url('plank.png')";
+    // game_canvas.appendChild(testElement);
+
+    let maze = new Maze(0, 0 , window.innerWidth, window.innerHeight);
+})();
