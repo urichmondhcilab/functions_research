d5a6d458511e56e7c36086a35650542bf7351704
squireaa <squireaa1@gmail.com>
2025-02-06 18:25:44 -0500

    Added ability for chicken to move thorough the maze

--- maze/script.js
+++ maze/script.js
@@ -1,6 +1,15 @@
 /**
  * Immediately Invoked Function Expression that creates a maze object
  */
-(function(){
-    let maze = new Maze(0, 0 , window.innerWidth, window.innerHeight);
-})();
+// (function(){
+//     let maze = new Maze(0, 0 , window.innerWidth, window.innerHeight);
+//     let chicken = new Chicken(maze);
+// })();
+
+/**
+ * The IIFE didn't work for whatever reason, so I use the DOMContentLoaded listener to do the same thing
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    let maze = new Maze(0, 0, window.innerWidth, window.innerHeight);
+    let chicken = new Chicken(maze);
+});
