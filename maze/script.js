afb96ceedddbf7d763cc96024841eace0fee478e
Mmachi Obiorah <obiorahmmachi@gmail.com>
2025-03-04 20:48:39 -0500

    delete maze folder

--- maze/script.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    // console.log(state[0]);
-    // let testElement = document.createElement("div");
-    // testElement.style.backgroundColor = "url('plank.png')";
-    // game_canvas.appendChild(testElement);
-
-    let maze = new Maze(0, 0 , window.innerWidth, window.innerHeight);
-})();
